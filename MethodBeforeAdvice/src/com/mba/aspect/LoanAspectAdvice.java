9c203bdc8e953020ecac46f023c8a80c077520c6
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-10 09:57:59 +0530

    With cross cutting logic

---
+++ MethodBeforeAdvice/src/com/mba/aspect/LoanAspectAdvice.java
@@ -0,0 +1,25 @@
+package com.mba.aspect;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class LoanAspectAdvice implements MethodBeforeAdvice {
+	@Override
+	public void before(Method method, Object[] args, Object proxy) throws Throwable {
+		String methodName = null;
+		methodName = method.getName();
+		System.out.println("---Auditing Report---\n"+new Date()+"\n\n");
+		System.out.println("The user1 accessed :"+methodName+"(");
+		for (int i = 0; i < args.length; i++) {
+			if (i==0) {
+				System.out.print(args[i]);
+				continue;
+			}
+			System.out.print(","+args[i]);
+		}
+		System.out.print(")");
+	}
+
+}

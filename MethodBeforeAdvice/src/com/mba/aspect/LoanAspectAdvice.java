3b5db300f78f2bbf07968fd96d7d2249340f5c52
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-11 11:15:38 +0530

    With Auditing logic

--- MethodBeforeAdvice/src/com/mba/aspect/LoanAspectAdvice.java
+++ MethodBeforeAdvice/src/com/mba/aspect/LoanAspectAdvice.java
@@ -5,16 +5,19 @@ import java.util.Date;
 
 import org.springframework.aop.MethodBeforeAdvice;
 
+import com.mba.security.helper.SecurityManager;
+
 public class LoanAspectAdvice implements MethodBeforeAdvice {
 	@Override
 	public void before(Method method, Object[] args, Object proxy) throws Throwable {
 		String methodName = null;
 		methodName = method.getName();
+		SecurityManager manager = SecurityManager.getInstance();
 		
 		args[0] = "LN00000001";
 		
 		System.out.println("---Auditing Report---\n"+new Date()+"\n\n");
-		System.out.print("The user1 accessed :"+methodName+"(");
+		System.out.print("The "+manager.getUnName() + " accessed :"+methodName+"(");
 		for (int i = 0; i < args.length; i++) {
 			if (i==0) {
 				System.out.print(args[i]);

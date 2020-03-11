36042a67a72fb01357af00cf9fef8b425c732c9e
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-11 11:17:15 +0530

    Final Testing

--- MethodBeforeAdvice/src/com/mba/test/MBATest.java
+++ MethodBeforeAdvice/src/com/mba/test/MBATest.java
@@ -3,16 +3,23 @@ package com.mba.test;
 import org.springframework.aop.framework.ProxyFactory;
 
 import com.mba.aspect.LoanAspectAdvice;
+import com.mba.aspect.SecurityCheckAdvice;
 import com.mba.beans.LoanManager;
+import com.mba.security.helper.SecurityManager;
 
 public class MBATest {
 	public static void main(String[] args) {
 		ProxyFactory factory = new ProxyFactory();
 		factory.setTarget(new LoanManager());
 		factory.addAdvice(new LoanAspectAdvice());
+		factory.addAdvice(new SecurityCheckAdvice());
 		
-		LoanManager manager = (LoanManager)factory.getProxy();
-		boolean status = manager.approveLoan("LN12345");
-		System.out.println("Loan Status : ? :"+status);
+		SecurityManager manager = SecurityManager.getInstance();
+		manager.login("mike", "welcome1");
+		
+		LoanManager loanManager = (LoanManager) factory.getProxy();
+		boolean status = loanManager.approveLoan("LN12345");
+		
+		System.out.println("Loan Status : ? :" + status);
 	}
 }

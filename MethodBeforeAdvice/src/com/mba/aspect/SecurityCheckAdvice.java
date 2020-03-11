b4a366ff430f861d526e62b01b7bfac47f74a11c
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-11 11:16:44 +0530

    with security check advice

---
+++ MethodBeforeAdvice/src/com/mba/aspect/SecurityCheckAdvice.java
@@ -0,0 +1,20 @@
+package com.mba.aspect;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import com.mba.security.helper.SecurityManager;
+
+public class SecurityCheckAdvice implements MethodBeforeAdvice {
+
+	@Override
+	public void before(Method method, Object[] args, Object proxy) throws Throwable {
+		SecurityManager securityManager = SecurityManager.getInstance();
+		boolean ststus = securityManager.authenticate();
+		if (ststus==false) {
+			throw new IllegalAccessException("Invalid username/pwd");
+		}
+
+	}
+}
